398fd0c25b9352d9e36c35239470f80c3dad1a93
guy3-lab <64708559+guy3-lab@users.noreply.github.com>
2023-05-23 20:37:01 -0700

    Update MatchingAlgorithm.java
    This file still has test stuff that are unnecessary and will need to be removed later.

--- app/src/main/java/com/idk/feetinder/MatchingAlgorithm.java
+++ app/src/main/java/com/idk/feetinder/MatchingAlgorithm.java
@@ -1,155 +1,194 @@
-/* Krish, commented out because doesn't compile yet and haven't had time to fix issues
-package com.idk.feetinder;
-
-import android.os.Build;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.*;
-import java.util.concurrent.CountDownLatch;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.database.*;
-
-public class MatchingAlgorithm {
-
-    public static void main(String[] args) throws InterruptedException {
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setDatabaseUrl("https://your-firebase-url.firebaseio.com/")
-                .setServiceAccount("path/to/serviceAccount.json")
-                .build();
-        FirebaseApp.initializeApp(options);
-        User user1 = collectUserData();
-        User user2 = collectUserData();
-        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users");
-        usersRef.child(user1.getId()).setValueAsync(user1);
-        usersRef.child(user2.getId()).setValueAsync(user2);
-        int k = 2;
-        List<User> users = getUsersFromFirebase();
-        List<User> neighbors1 = KNearestNeighbor.findKNearestNeighbors(user1, users, k);
-        List<User> neighbors2 = KNearestNeighbor.findKNearestNeighbors(user2, users, k);
-        Set<User> commonNeighbors = new HashSet<>(neighbors1);
-        commonNeighbors.retainAll(neighbors2);
-        System.out.println("User 1: " + user1);
-        System.out.println("User 2: " + user2);
-        System.out.println("Common neighbors: " + commonNeighbors);
-    }
-
-    public static User collectUserData() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.print("Enter your gender (male/female/other): ");
-        String gender = scanner.nextLine();
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt(); scanner.nextLine();
-        System.out.print("Enter your preferences (comma-separated): ");
-        String preferencesString = scanner.nextLine();
-        Set<String> preferences = new HashSet<>(Arrays.asList(preferencesString.split(","))); System.out.println();
-        return new User(name, preferences, gender, age);
-    }
-
-    public static List<User> getUsersFromFirebase() throws InterruptedException {
-        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("users");
-        List<User> users = new ArrayList<>();
-        CountDownLatch latch = new CountDownLatch(1);
-        usersRef.addListenerForSingleValueEvent(new ValueEventListener(){
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
-                    User user = childSnapshot.getValue(User.class);
-                    users.add(user);
-                }
-                latch.countDown();
-            }
-            public void onCancelled(DatabaseError databaseError){
-                System.out.println("Error reading from Firebase: " + databaseError.getMessage());
-                latch.countDown();
-            }
-        });
-        latch.await();
-        return users;
-    }
-}
-
-//user class - just represents user
 class User {
-    private String id;
     private String name;
-    private Set<String> preferences;
-    private String gender;
-    private int age;
-
-    public User(){}
-    public User(String name, Set<String> preferences, String gender, int age) {
-        this.id = UUID.randomUUID().toString();
+    private int toesPreference;
+    private String coffeePreference;
+    private String footSizePreference;
+    private String humanRightsPreference;
+    private String favoriteToe;
+    private int compatibilityScore;
+   
+    //idk if you need this cuz id from other codes takes this place??
+    //but this is just user instantiation
+    public User(String name, int toesPreference, String coffeePreference, String footSizePreference, String humanRightsPreference, String favoriteToe) {
         this.name = name;
-        this.preferences = preferences;
-        this.gender = gender;
-        this.age = age;
+        this.toesPreference = toesPreference;
+        this.coffeePreference = coffeePreference;
+        this.footSizePreference = footSizePreference;
+        this.humanRightsPreference = humanRightsPreference;
+        this.favoriteToe = favoriteToe;
+        this.compatibilityScore = 0;
     }
-
-    public String getId(){
-        return id;
+   
+    // need all of these
+    public void incrementCompatibilityScore(){
+        compatibilityScore++;
     }
-
-    public String getName(){
-        return name;
+    public int getCompatibilityScore(){
+        return compatibilityScore;
     }
-
-    public Set<String> getPreferences(){
-        return preferences;
+    public int getToesPreference(){
+        return toesPreference;
     }
-
-    public String getGender(){
-        return gender;
+    public String getCoffeePreference(){
+        return coffeePreference;
     }
-
-    public int getAge(){
-        return age;
+    public String getFootSizePreference(){
+        return footSizePreference;
     }
-
-    public String toString() {
-        return "User{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", preferences=" + preferences +
-                ", gender='" + gender + '\'' +
-                ", age=" + age +
-                '}';
+    public String getHumanRightsPreference(){
+        return humanRightsPreference;
     }
-
+    public String getFavoriteToe(){
+        return favoriteToe;
+    }
 }
 
+public class UserCompRank{ // class prob diff depending on what other code files say
+    private static List<User> userList;
+    private static User mainUser;
 
-    public static List<User> findKNearestNeighbors(User user, List<User> users, int k){
-        Map<User, Double> similarityScores = new HashMap<>();
-        for (User otherUser : users){
-            if (!user.equals(otherUser)){
-                double score = calculateSimilarityScore(user, otherUser);
-                similarityScores.put(otherUser, score);
+    //might need this to add user in initializeUsers or just use built in
+    //public void addUser(User user) userList.add(user);
+    
+
+// TEST (comment out if needed)
+    public static void main(String[] args) {
+        //initializeUsers();
+        calculateCompatibilityScores();
+        rankUsers();
+        //maybe not print?? because its just like an update in the app (idk)
+        //printValidity();
+    }
+    private static void initializeUsers() {
+        //these are just possible user which will obviosuly be removed once i understand how the id stuff in other files works
+        mainUser = new User("Main User", 3, "Black", "10 inches", "Right of work", "Long toe");
+        userList = new ArrayList<>();
+        userList.add(new User("User 1", 2, "Tea", "4 feet", "Freedom of expression", "Big toe"));
+	    userList.add(new User("User 2", 5, "Cappuccino", "13 inches", "Freedom of expression", "Long toe"));
+	    userList.add(new User("User 3", 1, "Latte", "26 yards", "Right to privacy", "Big toe"));
+	    userList.add(new User("User 4", 4, "Tea", "6 inches", "Right to education", "Pinky toe"));
+    }
+// END TEST
+
+
+
+//MAIN FUNCTION
+    private static void calculateCompatibilityScores() {
+        for (User user : userList) {
+            // q1:How many toes do you prefer?
+            if (user.getToesPreference() == mainUser.getToesPreference()) user.incrementCompatibilityScore();
+           
+            // q2:How do you like your coffee?
+            if (mainUser.getCoffeePreference().equals("Black") && user.getCoffeePreference().equals("Tea") ||
+                    mainUser.getCoffeePreference().equals("Black") && user.getCoffeePreference().equals("Black") ||
+                    mainUser.getCoffeePreference().equals("Tea") && user.getCoffeePreference().equals("Tea") ||
+                    mainUser.getCoffeePreference().equals("Tea") && user.getCoffeePreference().equals("Black") ||
+                    mainUser.getCoffeePreference().equals("Latte") &&
+                            (user.getCoffeePreference().equals("Latte") || user.getCoffeePreference().equals("Cappuccino") || user.getCoffeePreference().equals("Mocha")) ||
+                    mainUser.getCoffeePreference().equals("Cappuccino") &&
+                            (user.getCoffeePreference().equals("Cappuccino") || user.getCoffeePreference().equals("Latte") || user.getCoffeePreference().equals("Mocha")) ||
+                    mainUser.getCoffeePreference().equals("Mocha") &&
+                            (user.getCoffeePreference().equals("Mocha") || user.getCoffeePreference().equals("Latte") || user.getCoffeePreference().equals("Cappuccino"))) {
+                user.incrementCompatibilityScore();
+            }
+           
+            // q3:Ideal foot size?
+            int mainFootSize = convertFootSize(mainUser.getFootSizePreference());
+            int userFootSize = convertFootSize(user.getFootSizePreference());
+            int footSizeDifference = Math.abs(mainFootSize - userFootSize);
+            if (footSizeDifference == getMinimumFootSizeDifference()) {
+                user.incrementCompatibilityScore();
+            }
+           
+            // q4:What are the most important universal human rights?
+            if (mainUser.getHumanRightsPreference().equals("Right to work") && user.getHumanRightsPreference().equals("Right to education") ||
+                    mainUser.getHumanRightsPreference().equals("Right to work") && user.getHumanRightsPreference().equals("Right to work") ||
+                    mainUser.getHumanRightsPreference().equals("Right to education") && user.getHumanRightsPreference().equals("Right to education") ||
+                    mainUser.getHumanRightsPreference().equals("Right to education") && user.getHumanRightsPreference().equals("Right to work") ||
+                    mainUser.getHumanRightsPreference().equals("Right to life and liberty") &&
+                            (user.getHumanRightsPreference().equals("Right to life and liberty") || user.getHumanRightsPreference().equals("Right to privacy") || user.getHumanRightsPreference().equals("Freedom of expression")) ||
+                    mainUser.getHumanRightsPreference().equals("Right to privacy") &&
+                            (user.getHumanRightsPreference().equals("Right to privacy") || user.getHumanRightsPreference().equals("Right to liberty") || user.getHumanRightsPreference().equals("Freedom of expression")) ||
+                    mainUser.getHumanRightsPreference().equals("Freedom of expression") &&
+                            (user.getHumanRightsPreference().equals("Freedom of expression") || user.getHumanRightsPreference().equals("Right to privacy") || user.getHumanRightsPreference().equals("Right to life and liberty"))) {
+                user.incrementCompatibilityScore();
+            }
+
+            // q5:Favorite toe?
+            int mainFavoriteToe = convertToeNumber(mainUser.getFavoriteToe());
+            int userFavoriteToe = convertToeNumber(user.getFavoriteToe());
+            int toeDifference = Math.abs(mainFavoriteToe - userFavoriteToe);
+            if (toeDifference == getMinimumToeDifference()) user.incrementCompatibilityScore();
+        }
+    }
+    //simple converting for strings for easier comparison in foot size function
+    private static int convertFootSize(String footSize) {
+        Map<String, Integer> footSizeMap = new HashMap<>();
+        footSizeMap.put("6 inches", 6);
+        footSizeMap.put("10 inches", 10);
+        footSizeMap.put("13 inches", 13);
+        footSizeMap.put("4 feet", 48);
+        footSizeMap.put("26 yards", 936);
+        return footSizeMap.get(footSize);
+    }
+    
+    //same as above but for which toe user likes
+    private static int convertToeNumber(String toe) {
+        Map<String, Integer> toeNumberMap = new HashMap<>();
+        toeNumberMap.put("Big toe", 5);
+        toeNumberMap.put("Long toe", 4);
+        toeNumberMap.put("Middle toe", 3);
+        toeNumberMap.put("Ring toe", 2);
+        toeNumberMap.put("Pinky toe", 1);
+        return toeNumberMap.get(toe);
+    }
+    
+    //get diff to comapre to other user diff
+    private static int getMinimumFootSizeDifference() {
+        int minDifference = Integer.MAX_VALUE;
+        for (User user : userList) {
+            int mainFootSize = convertFootSize(mainUser.getFootSizePreference());
+            int userFootSize = convertFootSize(user.getFootSizePreference());
+            int footSizeDifference = Math.abs(mainFootSize - userFootSize);
+            if (footSizeDifference < minDifference) {
+                minDifference = footSizeDifference;
             }
         }
-
-        //sort users by similarity scores
-        List<User> sortedUsers = new ArrayList<>(users);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            sortedUsers.sort((u1, u2) -> Double.compare(similarityScores.get(u2), similarityScores.get(u1)));
+        return minDifference;
+    }
+    
+    //same as above but toe diffs
+    private static int getMinimumToeDifference() {
+        int minDifference = Integer.MAX_VALUE;
+        for (User user : userList) {
+            int mainFavoriteToe = convertToeNumber(mainUser.getFavoriteToe());
+            int userFavoriteToe = convertToeNumber(user.getFavoriteToe());
+            int toeDifference = Math.abs(mainFavoriteToe - userFavoriteToe);
+            if (toeDifference < minDifference) {
+                minDifference = toeDifference;
+            }
         }
-        return sortedUsers.subList(0, Math.min(k, sortedUsers.size()));
+        return minDifference;
     }
+//END OF MAIN FUNCTION
+   
+   
+    //sorts all users so a final update
+    public static void rankUsers(){ userList.sort(Comparator.comparingInt(User::getCompatibilityScore).reversed());}
 
 
-    private static double calculateSimilarityScore(User user1, User user2) {
-        Set<String> intersection = new HashSet<>(user1.getPreferences());
-        intersection.retainAll(user2.getPreferences());
-        Set<String> union = new HashSet<>(user1.getPreferences());
-        union.addAll(user2.getPreferences());
-        double jIndex = (double) intersection.size() / union.size();
-        double ageDiff = Math.abs(user1.getAge() - user2.getAge());
-        double genderDiff = user1.getGender().equals(user2.getGender()) ? 0.0 : 1.0;
-        double eDist = Math.sqrt(ageDiff * ageDiff + genderDiff * genderDiff);
-        //finally return similarity score (took too long)
-        return jIndex * (1.0 / (1.0 + eDist));
-    }
-
+    //TEST (comment out if needed)
+    /*public static void printValidity() {
+        // System.out.println("Compatibility Ranking:");
+        // for (int i = 0; i < userList.size(); i++) {
+        //     User user = userList.get(i);
+        //     System.out.println((i + 1) + ". " + user);
+        // }
+    }*/
 }
-*/
